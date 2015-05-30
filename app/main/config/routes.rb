091526ec8e91936d4acf89ffcaacc7238cb68367
Ian Donovan <idonovan90@gmail.com>
2015-05-30 16:03:26 -0400

    Remove login for now

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -2,10 +2,6 @@
 
 client '/about', action: 'about'
 
-# Routes for login and signup, provided by user_templates component gem
-client '/signup', component: 'user_templates', controller: 'signup'
-client '/login', component: 'user_templates', controller: 'login'
-
 # The main route, this should be last. It will match any params not
 # previously matched.
 client '/', {}
